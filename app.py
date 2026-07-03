0781e2178815f6f70be3456ae1cfa434223f841e
exbanny58-alt <@.com>
2026-07-03 22:49:28 +0300

    чня

--- app.py
+++ app.py
@@ -209,4 +209,4 @@ def browse_folder():
 
 if __name__ == '__main__':
     print("🚀 Сервер запущен: http://127.0.0.1:5000")
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
