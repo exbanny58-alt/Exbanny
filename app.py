850578783178df68ab579f7f43d236a6808002a9
exbanny58-alt <@.com>
2026-07-04 19:32:31 +0300

    Шлифовка стиля(Иконки)

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from routes import register_routes
-from flaskwebgui import FlaskUI
 import logging
 import sys
 
@@ -10,19 +9,25 @@ app = Flask(__name__)
 register_routes(app)
 
 if __name__ == '__main__':
-    # Отключаем логи FlaskWebGUI
-    flaskwebgui_logger = logging.getLogger('flaskwebgui')
-    flaskwebgui_logger.setLevel(logging.ERROR)
-    
-    # Отключаем логи от werkzeug (это логи Flask)
-    # Если хотите оставить стандартные логи Flask - закомментируйте эти строки
-    # log = logging.getLogger('werkzeug')
-    # log.setLevel(logging.ERROR)
-    
-    FlaskUI(
-        app=app,
-        server="flask",
-        width=1300,
-        height=850,
-        fullscreen=False
-    ).run()
+    # Проверяем аргументы командной строки
+    if '--gui' in sys.argv:
+        # Запуск в GUI режиме
+        from flaskwebgui import FlaskUI
+        
+        flaskwebgui_logger = logging.getLogger('flaskwebgui')
+        flaskwebgui_logger.setLevel(logging.ERROR)
+        
+        FlaskUI(
+            app=app,
+            server="flask",
+            width=1300,
+            height=850,
+            fullscreen=False
+        ).run()
+    else:
+        # Запуск в браузере (по умолчанию)
+        app.run(
+            host='127.0.0.1',
+            port=5000,
+            debug=True
+        )
